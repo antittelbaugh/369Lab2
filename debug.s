be37ac5a4c0c8f729d1c5b8b6262200f2a08beb3
antittelbaugh <85364208+antittelbaugh@users.noreply.github.com>
2022-09-12 13:19:51 -0700

    Add files via upload

--- debug.s
+++ debug.s
@@ -1,18 +1,18 @@
 # Exercise 2
 #  Max Score: 15 points
 #
-# Students:
+# Students: 
 #
-# 'count_occurence.a' - count the occurrences of a specific character in string
+# 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
 #  MAX Score: 15 points
-# Expected Outcome:-
+# Expected Outcome:- 
 # The following string will be printed on the console,
 # "Count is 6"
 #
 # Questions:-
 # 1. Briefly describe the purposes of the registers, $t0, $t1, $t2, and $t3.
-# 2. Currently, the program is stuck in an infinite loop. Make use of
+# 2. Currently, the program is stuck in an infinite loop. Make use of 
 #    breakpoints to locate, and correct the error.
 
 	.text
@@ -28,13 +28,13 @@ main:
 loop:
 
     lb      $t0, str($t1)	# fetch a character in 'str'
-	beqz    $t0, out	    # if a null character is fetched, exit the loop
-	bne    $t0, $t3, con   # branches to 'con' if registers $t0, and $t3 are not the same
-	add     $t2, $t2, 1	    # increment register $t2
+	beqz    $t0, strEnd	    # if a null character is fetched, exit the loop
+	bne     $t0, $t3, con   # branches to 'con' if registers $t0, and $t3 are not the same
+	addi    $t2, $t2, 1	    # increment register $t2
 
 con:	
 
-    add     $t2, $t1, 1	    # increase indexing register $t1
+    addi     $t1, $t1, 1	    # increase indexing register $t1
 	j       loop	       	# continues the loop
 
 strEnd:
